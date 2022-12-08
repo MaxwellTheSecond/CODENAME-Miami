63594ebd1c98ac2d45aae6508c8827e8b43107bb
Grigore Robert George <grigorerobertgeorge@yahoo.com>
2022-12-08 17:19:02 +0200

    V1.0.3
    Added dissolve effect on death
    Known bugs: weapon sway causes weapon to glitch

--- Assets/Scripts/Target.cs
+++ Assets/Scripts/Target.cs
@@ -4,18 +4,38 @@ using UnityEngine;
 
 public class Target : MonoBehaviour
 {
-    public float health = 100f;
+    [SerializeField] Shader _dissolveShader;
+    [SerializeField] float _dissolveDuration = 1f;
+    [SerializeField] float _health = 100f;
     public void TakeDamage(float amount)
     {
-        health-=amount;
-        if(health<=0)
+        _health-=amount;
+        if(_health<=0)
         {
             Die();
         }
     }
-
     private void Die()
+    {   
+        Color oldColor = gameObject.GetComponent<Renderer>().material.color;
+        Material newMaterial = gameObject.GetComponent<Renderer>().material;
+        newMaterial.shader = _dissolveShader;
+        gameObject.GetComponent<Renderer>().material=newMaterial;
+        //gameObject.GetComponent<Renderer>().sharedMaterial.SetColor("_OldColor",oldColor);
+        gameObject.GetComponent<Renderer>().material.SetFloat("_DissolveAmount",0f);
+        StartCoroutine("Dissolve");
+    }
+
+    IEnumerator Dissolve()
     {
+        float dissolveAmount = 0f;
+        while(dissolveAmount<1f)
+        {
+            dissolveAmount += 0.01f;
+            dissolveAmount = Mathf.Clamp01(dissolveAmount);
+            gameObject.GetComponent<Renderer>().material.SetFloat("_DissolveAmount",dissolveAmount);
+            yield return new WaitForSeconds(_dissolveDuration/100f);
+        }
         Destroy(gameObject);
     }
 }
